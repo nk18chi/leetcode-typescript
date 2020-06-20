d85f0289eb6f975454cde5a00d86f2378235d519
Naoki Mita <naoki.mita18@gmail.com>
2020-06-20 16:48:55 -0700

    Add templete files

---
+++ template/index.ts
@@ -0,0 +1,10 @@
+// id
+// url
+
+// import { TreeNode } from "../../data_structure/treeNode";
+
+// Time complexity: O()
+// Space complexity: O()
+export function func(arg1: number): number {
+  return arg1;
+}
