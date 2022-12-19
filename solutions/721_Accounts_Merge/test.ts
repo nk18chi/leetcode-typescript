fce57638d9cd6c7ef16c2451421cee5cfd48618b
Naoki Mita <naoki.mita18@gmail.com>
2022-12-19 12:52:31 -0800

    721. Accounts Merge

---
+++ solutions/721_Accounts_Merge/test.ts
@@ -0,0 +1,48 @@
+import { accountsMerge } from "./index";
+
+const assert = require("assert");
+
+interface ITestCase {
+  arg1: string[][];
+  result: string[][];
+}
+
+describe("#accountsMerge", () => {
+  const tests: ITestCase[] = [
+    {
+      arg1: [
+        ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
+        ["John", "johnsmith@mail.com", "john00@mail.com"],
+        ["Mary", "mary@mail.com"],
+        ["John", "johnnybravo@mail.com"],
+      ],
+      result: [
+        ["John", "john00@mail.com", "john_newyork@mail.com", "johnsmith@mail.com"],
+        ["Mary", "mary@mail.com"],
+        ["John", "johnnybravo@mail.com"],
+      ],
+    },
+    {
+      arg1: [
+        ["Gabe", "Gabe0@m.co", "Gabe3@m.co", "Gabe1@m.co"],
+        ["Kevin", "Kevin3@m.co", "Kevin5@m.co", "Kevin0@m.co"],
+        ["Ethan", "Ethan5@m.co", "Ethan4@m.co", "Ethan0@m.co"],
+        ["Hanzo", "Hanzo3@m.co", "Hanzo1@m.co", "Hanzo0@m.co"],
+        ["Fern", "Fern5@m.co", "Fern1@m.co", "Fern0@m.co"],
+      ],
+      result: [
+        ["Gabe", "Gabe0@m.co", "Gabe1@m.co", "Gabe3@m.co"],
+        ["Kevin", "Kevin0@m.co", "Kevin3@m.co", "Kevin5@m.co"],
+        ["Ethan", "Ethan0@m.co", "Ethan4@m.co", "Ethan5@m.co"],
+        ["Hanzo", "Hanzo0@m.co", "Hanzo1@m.co", "Hanzo3@m.co"],
+        ["Fern", "Fern0@m.co", "Fern1@m.co", "Fern5@m.co"],
+      ],
+    },
+  ];
+  let i = 0;
+  for (let { arg1, result } of tests) {
+    it(`test${i++}`, () => {
+      assert.deepStrictEqual(accountsMerge(arg1), result);
+    });
+  }
+});
